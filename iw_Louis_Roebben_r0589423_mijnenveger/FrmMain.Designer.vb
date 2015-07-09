6940aa955f10e0623bd62c951bf7f724d8e96e8c
covert8 <the.deller@gmail.com>
2015-07-09 17:35:47 +0200

    wip
    done: construction playingfield; planting bombs; calculation numbers
    tbd: reticulation splines

---
+++ iw_Louis_Roebben_r0589423_mijnenveger/FrmMain.Designer.vb
@@ -0,0 +1,92 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.ToolStripStatusLabel1 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.lblTime = New System.Windows.Forms.Label()
+        Me.StatusStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(839, 13)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(218, 37)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripStatusLabel1})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 726)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(1069, 30)
+        Me.StatusStrip1.TabIndex = 1
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'ToolStripStatusLabel1
+        '
+        Me.ToolStripStatusLabel1.Name = "ToolStripStatusLabel1"
+        Me.ToolStripStatusLabel1.Size = New System.Drawing.Size(184, 25)
+        Me.ToolStripStatusLabel1.Text = "ToolStripStatusLabel1"
+        '
+        'Timer1
+        '
+        '
+        'lblTime
+        '
+        Me.lblTime.AutoSize = True
+        Me.lblTime.Location = New System.Drawing.Point(976, 726)
+        Me.lblTime.Name = "lblTime"
+        Me.lblTime.Size = New System.Drawing.Size(81, 20)
+        Me.lblTime.TabIndex = 2
+        Me.lblTime.Text = "Time: XXs"
+        '
+        'FrmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1069, 756)
+        Me.Controls.Add(Me.lblTime)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "FrmMain"
+        Me.Text = "FrmMain"
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents StatusStrip1 As System.Windows.Forms.StatusStrip
+    Friend WithEvents ToolStripStatusLabel1 As System.Windows.Forms.ToolStripStatusLabel
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents lblTime As System.Windows.Forms.Label
+
+End Class
---
+++ iw_Louis_Roebben_r0589423_mijnenveger/FrmMain.vb
@@ -0,0 +1,104 @@
+﻿Public Class FrmMain
+    'Matrix van 8 breed (0 tot en met 7) en 8 hoog (0 tot en met 7)
+    Dim oRooster(7, 7) As Button
+    Dim iTime As Long = 0
+    Dim iUnturned
+    Private Sub FrmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        For iRij As Integer = oRooster.GetLowerBound(0) To oRooster.GetUpperBound(0)
+            For iKolom As Integer = oRooster.GetLowerBound(1) To oRooster.GetUpperBound(1)
+                oRooster(iRij, iKolom) = New Button
+                oRooster(iRij, iKolom).AutoSize = False
+                oRooster(iRij, iKolom).Size = New Size(50, 50)
+                oRooster(iRij, iKolom).TextAlign = ContentAlignment.MiddleCenter
+                oRooster(iRij, iKolom).Font = New Font("Arial", 12, FontStyle.Bold)
+                oRooster(iRij, iKolom).Name = "cLbl_" & iRij & "," & iKolom
+                'voor herinterpretatie vatbaar
+                'x,y locatie in het venster
+                Dim iXLocatie, iYLocatie As Integer
+                iXLocatie = (iRij * 50) + 10
+                'debug.WriteLine(iXLocatie)
+                iYLocatie = (iKolom * 50) + 10
+                'debug.WriteLine(iYLocatie)
+                oRooster(iRij, iKolom).Location = New Point(iYLocatie, iXLocatie)
+
+                'kleur van de tegel
+                oRooster(iRij, iKolom).BackColor = System.Drawing.Color.LightGray
+                'text op de tegel (initieel leeg)
+                oRooster(iRij, iKolom).Text = ""
+                Me.Controls.Add(oRooster(iRij, iKolom))
+
+                'Voor ieder klikbaar Label:
+                'linker klik
+                AddHandler oRooster(iRij, iKolom).Click, AddressOf FncOnLabelClick
+            Next
+        Next
+        Timer1.Start()
+        'verstop 10 mijnen
+        FncVerstopMijnen()
+
+    End Sub
+    Private Sub FncVerstopMijnen()
+        Dim rand As Random = New Random
+        Dim iRij, iKolom As Integer
+        For i As Integer = 0 To 9
+            iRij = rand.Next(oRooster.GetLowerBound(0), oRooster.GetUpperBound(0))
+            iKolom = rand.Next(oRooster.GetLowerBound(0), oRooster.GetUpperBound(0))
+            If (oRooster(iRij, iKolom).Tag = -1) Then i -= 1
+
+            oRooster(iRij, iKolom).Tag = -1
+        Next
+    End Sub
+    Private Sub FncBerekenGetallen()
+        For iRij As Integer = oRooster.GetLowerBound(0) To oRooster.GetUpperBound(0)
+            For iKolom As Integer = oRooster.GetLowerBound(1) To oRooster.GetUpperBound(1)
+                If (oRooster(iRij, iKolom).Tag <> -1) Then
+                    oRooster(iRij, iKolom).Tag = FncGetNeighbourValues(iRij, iKolom)
+                End If
+
+            Next
+        Next
+    End Sub
+    Private Function FncGetNeighbourValues(i As Integer, j As Integer)
+        Dim iSom As Integer
+        For iRij As Integer = i - 1 To i + 1
+            For iKolom As Integer = j - 1 To j + 1
+                If (FncCheckCoord(iRij, iKolom)) Then
+                    If(oRooster(iRij,iKolom).Tag == -1) iSom += 1
+                    End If
+                End If
+            Next
+        Next
+        Return iSom
+    End Function
+    Private Function FncCheckCoord(i As Integer, j As Integer)
+        If (i < 0 Or i > 8) Then Return False
+        If (j < 0 Or j > 8) Then Return False
+        Return True
+    End Function
+
+    Private Sub FncOnLabelClick(ByVal sender As Button, ByVal e As MouseEventArgs)
+        If (e.Button = Windows.Forms.MouseButtons.Left) Then
+            If (sender.Text = "X") Then Return
+            If (sender.Tag = -1) Then
+                FncGameOver()
+            Else
+                sender.Text = sender.Tag
+            End If
+        ElseIf (e.Button = Windows.Forms.MouseButtons.Right) Then
+
+        Else
+
+        End If
+
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        iTime += 1
+        lblTime.Text = "Time: " & Int(iTime / 10) & "s"
+    End Sub
+
+    Private Sub FncGameOver()
+
+    End Sub
+End Class
